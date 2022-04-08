16d452ac5629bdc134e8cbec72c622cfff3dca30
CustomCheat <60106212+CustomCheat@users.noreply.github.com>
2022-04-08 19:37:29 +0200

    Source code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.net.URISyntaxException;
+
+public class Main {
+    public static SearchFrame search;
+    public static void main(String[] args) throws URISyntaxException {
+        search = new SearchFrame();
+    }
+}
